c9d2f2feddb3c02867143bbb08967cb68aed9c8c
Artem Babushkin <4105440@gmail.com>
2025-11-16 22:06:11 +0300

    fix config safe load

--- generate.py
+++ generate.py
@@ -4,9 +4,8 @@ import yaml
 
 try:
     with open(argv[1], 'r') as f:
-        cfg = yaml.save_load(f)
+        cfg = yaml.safe_load(f)
 except Exception as ex:
-    print(f'Failed to load config file.\nUsage: {argv[0]} path-to-config-file.yaml\n\n')
-    raise ex
+    raise Exception(f'Failed to load config file. {ex}\nUsage: {argv[0]} path-to-config-file.yaml\n\n')
 
-
+cfg
