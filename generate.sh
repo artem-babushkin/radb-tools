257f565ca852361ef79e85c9252b9a4f02b6ba4d
Artem Babushkin <4105440@gmail.com>
2025-11-19 23:59:05 +0300

    fix generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
+cd "$(dirname $0)"
+printf 'Clear old archive\n'
 rm -f rib*.bz2
+printf 'Download ASN names\n'
 curl -O -L https://ftp.ripe.net/ripe/asnames/asn.txt
+printf 'Download RIB\n'
 pyasn_util_download.py --latest
+printf 'Convert RIB\n'
 pyasn_util_convert.py --single $(find . -maxdepth 1 -name "rib*.bz2" -print0) ipasn.lst
+printf 'Filter RIB\n'
 ./filter.py config.yaml
