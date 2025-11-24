dbe8d7919cd65e861a71722063b221226e137587
Artem Babushkin <4105440@gmail.com>
2025-11-24 21:52:02 +0300

    fix: add timeout to download

--- generate.sh
+++ generate.sh
@@ -3,7 +3,7 @@ cd "$(dirname $0)"
 printf 'Clear old archive\n'
 rm -f rib*.bz2
 printf 'Download ASN names\n'
-curl -O -L https://ftp.ripe.net/ripe/asnames/asn.txt
+timeout 60 curl -O -L https://ftp.ripe.net/ripe/asnames/asn.txt || exit 1
 printf 'Download RIB\n'
 pyasn_util_download.py --latest
 printf 'Convert RIB\n'
