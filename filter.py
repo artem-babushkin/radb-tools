2298e491f47f166d3c9175076c76def9acad3773
Artem Babushkin <4105440@gmail.com>
2025-11-24 23:03:50 +0300

    dev: filter. Parse AS names

--- filter.py
+++ filter.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 from sys import argv
 import yaml
+import re
+import pyasn
+from aggregate_prefixes import aggregate_prefixes
 import logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level = logging.DEBUG)
@@ -9,14 +12,47 @@ try:
     with open(argv[1], 'r') as f:
         CONFIG = yaml.safe_load(f)
     logger.info(f'Config loaded from {argv[1]}')
-    logger.debug(f'{CONFIG}')
 except Exception as ex:
     text = f'Failed to load config file. {ex}'
     logger.error(text)
     raise Exception(f'{text}\nUsage: {argv[0]} path-to-config-file.yaml\n\n')
 
+logger.debug(f'{CONFIG}')
+
 def main():
     print('Hi there!')
+
+    # head ipasn.lst  (asn-db)
+    #1.0.6.0/24	38803
+    #
+    # head asn.txt    (asnames) 
+    #0 -Reserved AS-, ZZ
+    #1 LVLT-1, US
+
+    workdir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    all_ip_asn = pyasn.pyasn(f'{workdir}/{CONFIG["asn-db"]}')
+    with open(f'{workdir}/filtered-prefixes.txt', 'w') as out, open(f'{workdir}/{CONFIG["asnames-file"]}') as all_as_names:
+        target_prefixes = [CONFIG['prefix']]
+        target_asnumbers = [CONFIG['asn']]
+        asname_regex_patterns = [re.compile(i) for i in CONFIG['as-name-regex']]
+        logger.debug('Parse AS names file')
+        for line in all_as_names:
+            try:
+                asnumber, asowner = line.split()
+                asname, ascountry = asowner.split(',')
+                for country in CONFIG['country']:
+                    if country.lower() == ascountry.strip().lower():
+                        logger.debug(f'Country {country} match. AS name: {asname}')
+                        target_asnumbers += asnumber
+                for pattern in asname_regex_patterns:
+                    if pattern.match(asname, re.IGNORECASE):
+                        logger.debug(f'AS name regex {pattern.pattern} match. AS name: {asname}')
+                        target_asnumbers += asnumber
+            except Exception as ex:
+                logger.warn(f'Failed to parse AS names file line {line.strip()}. {ex}')
+
+
+
 
 
 if __name__=='__main__':
--- generate.sh
+++ generate.sh
@@ -3,9 +3,9 @@ cd "$(dirname $0)"
 printf 'Clear old archive\n'
 rm -f rib*.bz2
 printf 'Download ASN names\n'
-timeout 60 curl -O -L https://ftp.ripe.net/ripe/asnames/asn.txt || exit 1
+curl -O -L https://ftp.ripe.net/ripe/asnames/asn.txt || exit 1
 printf 'Download RIB\n'
-timeout 60 pyasn_util_download.py --latest
+timeout 600 pyasn_util_download.py --latest
 printf 'Convert RIB\n'
 pyasn_util_convert.py --single $(find . -maxdepth 1 -name "rib*.bz2" -print0) ipasn.lst
 printf 'Filter RIB\n'
