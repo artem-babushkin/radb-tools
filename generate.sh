4552009940157a5f1e9d5a56a6008dcb17beeebb
Artem Babushkin <4105440@gmail.com>
2025-11-24 23:50:21 +0300

    dev: add log to generate.sh

--- generate.sh
+++ generate.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 cd "$(dirname $0)"
+printf 'Working dir content:\n'
+pwd
+ls -l
 if [[ $RADBKEEPCACHE == 0 ]]; then
   printf 'Clear cache\n'
   rm -rf cache
   #rm -f rib*.bz2
 fi
 mkdir -p cache && cd cache
+printf 'Cache content:\n'
+pwd
+ls -l
 if [[ ! -f asn.txt ]]; then
   printf 'Download ASN names\n'
   curl -O -L https://ftp.ripe.net/ripe/asnames/asn.txt || exit 1
@@ -16,6 +22,9 @@ if [[ ! -f ipasn.lst ]]; then
   printf 'Convert RIB\n'
   pyasn_util_convert.py --single $(find . -maxdepth 1 -name "rib*.bz2" -print0) ipasn.lst
 fi
+printf 'Cache content after download:\n'
+pwd
+ls -l
 cd ..
 printf 'Filter RIB\n'
 ./filter.py config.yaml
